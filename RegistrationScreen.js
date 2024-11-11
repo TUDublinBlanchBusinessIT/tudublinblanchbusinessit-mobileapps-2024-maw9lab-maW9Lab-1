f69af218f239d6914c4612c53371039223b4e029
NojusIlekis <158283465+NojusIlekis@users.noreply.github.com>
2024-11-11 16:14:30 +0000

    Update RegistrationScreen.js

--- RegistrationScreen.js
+++ RegistrationScreen.js
@@ -3,16 +3,64 @@ import { View, Text, TextInput, Button } from 'react-native';
 import styles from '../assets/appstyles';
 
 export default function RegistrationScreen() {
+  const [firstname, setFirstname] = useState('');
+  const [surname, setSurname] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [county, setCounty] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
 
-  const handleRegister = () => {
-    alert(`Registered with username: ${username}, email: ${email}`);
-  };
+    const handleRegister = () => {
+    alert(
+      `Registered with:\n
+      Firstname: ${firstname}\n
+      Surname: ${surname}\n
+      Street: ${street}\n
+      City: ${city}\n
+      County: ${county}\n
+      Username: ${username}\n
+      Email: ${email}`
+    );
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Register</Text>
+            <TextInput
+        style={styles.input}
+        placeholder="Firstname"
+        value={firstname}
+        onChangeText={setFirstname}
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Surname"
+        value={surname}
+        onChangeText={setSurname}
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="Street"
+        value={street}
+        onChangeText={setStreet}
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="City"
+        value={city}
+        onChangeText={setCity}
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="County"
+        value={county}
+        onChangeText={setCounty}
+      />
       <TextInput
         style={styles.input}
         placeholder="Username"
